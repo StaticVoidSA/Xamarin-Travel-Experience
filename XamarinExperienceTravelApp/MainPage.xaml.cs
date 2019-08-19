5091059d7a9740c34d890327c3cfd65ef6efe138
Jonathan <31016109+StaticVoidSA@users.noreply.github.com>
2019-08-19 09:41:30 +0200

    Update MainPage.xaml.cs

--- XamarinExperienceTravelApp/MainPage.xaml.cs
+++ XamarinExperienceTravelApp/MainPage.xaml.cs
@@ -16,16 +16,28 @@ namespace XamarinExperienceTravelApp
 
         private void LoginBtn_Clicked(object sender, EventArgs e)
         {
-            bool isEmailEmpty = string.IsNullOrEmpty(emailTxt.Text);
-            bool isPasswordEmpty = string.IsNullOrEmpty(passwordTxt.Text);
-
-            if (isEmailEmpty || isPasswordEmpty)
+            if (ValidateInput())
             {
                 DisplayAlert("Error", "Please enter email and password.", "OK");
             }
             else
             {
                 Navigation.PushAsync(new HomePage());
+            }
+        }
+        
+        public bool ValidateInput()
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailTxt.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordTxt.Text);
+            
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+                return true; 
+            }
+            else
+            {
+                return false;
             }
         }
     }
